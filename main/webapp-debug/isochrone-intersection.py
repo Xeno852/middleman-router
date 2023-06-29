9c280858883f40abd775ca09cb7fc949cd8af156
Xeno852 <lukeblackwell@protonmail.com>
2023-06-29 02:00:18 -0400

    Working increasing radius (need to work on decreasing next)

--- main/webapp-debug/isochrone-intersection.py
+++ main/webapp-debug/isochrone-intersection.py
@@ -15,52 +15,70 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
 @app.route('/get_intersection', methods=['POST'])
 def get_intersection():
     nodes = request.json.get('nodes')
     show_isochrones = request.json.get('show_isochrones')
     starting_radius = int(request.json.get('starting_radius', 500))
     radius_increment = int(request.json.get('radius_increment', 500))
+    max_distance = int(request.json.get('max_distance', 10000))
     
     client = openrouteservice.Client(key=ors_api_key)
-    
-    isochrones = []
     
     logging.info(f'Received nodes: {nodes}')
     logging.info(f'Show isochrones: {show_isochrones}')
     logging.info(f'Starting Radius: {starting_radius}')
     logging.info(f'Radius Increment: {radius_increment}')
+    logging.info(f'Max Distance: {max_distance}')
 
-    # Collect isochrones for all nodes
-    for node in nodes:
-        logging.info(f'Processing node: {node}')
-        isochrone = client.isochrones(
-            locations=[node],
-            profile='foot-walking',
-            range=[starting_radius],
-        )
-        isochrone_geom = shapely.geometry.shape(isochrone['features'][0]['geometry'])
-        isochrones.append(isochrone_geom)
-    
-    # Calculate intersection
-    if isochrones:
-        intersection = isochrones[0]
-        for isochrone in isochrones[1:]:
-            intersection = intersection.intersection(isochrone)
-            if intersection.is_empty:
+    radius = starting_radius
+    intersection = None
+
+    # Keep trying with an increased radius until an intersection is found or max_distance is reached
+    while radius <= max_distance:
+        isochrones = []
+        # Collect isochrones for all nodes with the current radius
+        for node in nodes:
+            logging.info(f'Processing node: {node}')
+            isochrone = client.isochrones(
+                locations=[node],
+                profile='foot-walking',
+                range=[radius],
+            )
+            isochrone_geom = shapely.geometry.shape(isochrone['features'][0]['geometry'])
+            isochrones.append(isochrone_geom)
+
+        # Calculate intersection
+        if isochrones:
+            intersection = isochrones[0]
+            for isochrone in isochrones[1:]:
+                intersection = intersection.intersection(isochrone)
+                if intersection.is_empty:
+                    break
+            
+            logging.info(f'Intersection geometry: {intersection}')
+
+            # If intersection is found, break the loop
+            if not intersection.is_empty:
                 break
         
-        logging.info(f'Intersection geometry: {intersection}')
-        
-        if not intersection.is_empty:
-            final_isochrones = [shapely.geometry.mapping(iso) for iso in isochrones]
-            intersection_geometry = shapely.geometry.mapping(intersection)
-            logging.info(f'Returning intersection geometry: {intersection_geometry}')
-            logging.info(f'Final isochrones: {final_isochrones}')
-            return jsonify({"geometry": intersection_geometry, "final_isochrones": final_isochrones if show_isochrones else None})
+        # Increment radius
+        radius += radius_increment
+    
+    # Return intersection if found
+    if intersection and not intersection.is_empty:
+        final_isochrones = [shapely.geometry.mapping(iso) for iso in isochrones]
+        intersection_geometry = shapely.geometry.mapping(intersection)
+        logging.info(f'Returning intersection geometry: {intersection_geometry}')
+        logging.info(f'Final isochrones: {final_isochrones}')
+        return jsonify({"geometry": intersection_geometry, "final_isochrones": final_isochrones if show_isochrones else None})
     
     logging.info('No intersection found')
     return jsonify({"geometry": None, "final_isochrones": None})
+
+
+
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
